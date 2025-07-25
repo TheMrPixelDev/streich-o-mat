fcead3c6689aee084e5a7cdf1c4f1852b71eb79f
Julian Harrer <julian.harrer@posteo.de>
2025-07-25 14:13:55 +0200

    added cors rules

--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from extract_grades import extract_grades_from_pdf
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000"
+    "http://streich-o-mat.pxldeveloper.eu"
+]
+
+app.add_middleware(CORSMiddleware,allow_origins=origins, allow_methods=["*"], allow_headers=["*"], allow_credentials=True)
 
 @app.post("/parse_pdf")
 async def upload_pdf(file: UploadFile = File()):
